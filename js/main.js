3a976bb4dfae13bf9c00b5c206fa99427e980a35
MarufAlAslam <55714750+MarufAlAslam@users.noreply.github.com>
2022-09-07 16:28:33 +0600

    local storage revised

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
 // check local storage is Nan or not
-if (isNaN(localStorage.getItem('value'))) {
-    localStorage.setItem('value', 0);
-};
+// localStorage.setItem('value', (isNaN(localStorage.getItem('value') ? 0 : 0)));
+
 
 // get value from localstorage and put that into the counter
 
 const value = document.getElementById('value');
 const localStorageValue = localStorage.getItem('value');
-value.innerText = localStorageValue;
 localStorage.setItem('value', parseInt(value.innerText) === NaN ? 0 : parseInt(value.innerText));
 
 // convert the value to a number
@@ -24,3 +22,5 @@ const dec = () => {
     value.innerText = decreasedValue;
     localStorage.setItem('value', decreasedValue);
 }
+
+value.innerText = localStorageValue;
